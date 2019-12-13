d52e9b6b0dd6806432042a90cbc665d2f7a04ce2
Malar Kandasamy <k.malarvizhi@in.ibm.com>
2019-12-13 12:21:59 +0530

    authorization policy changes

--- image.tf
+++ image.tf
@@ -23,21 +23,41 @@ locals {
 # Create IAM Authorization Policy for user to able to create custom image
 # pointing to COS object url hosted in vnfsvc account.
 ##############################################################################
-resource "ibm_iam_authorization_policy" "authorize_image" {
-  depends_on                  = ["data.ibm_is_vpc.f5_vpc"]
-  provider                    = "ibm.vfnsvc"
-  source_service_account      = "${local.user_acct_id}"
-  source_service_name         = "is"
-  source_resource_type        = "image"
-  target_service_name         = "cloud-object-storage"
-  target_resource_type        = "bucket"
-  roles                       = ["Reader"]
-  target_resource_instance_id = "${var.vnf_f5bigip_cos_instance_id}"
+#resource "ibm_iam_authorization_policy" "authorize_image" {
+#  depends_on                  = ["data.ibm_is_vpc.f5_vpc"]
+#  provider                    = "ibm.vfnsvc"
+#  source_service_account      = "${local.user_acct_id}"
+#  source_service_name         = "is"
+#  source_resource_type        = "image"
+#  target_service_name         = "cloud-object-storage"
+#  target_resource_type        = "bucket"
+#  roles                       = ["Reader"]
+#  target_resource_instance_id = "${var.vnf_f5bigip_cos_instance_id}"
+#}
+
+# IAM Authorization to create custom images
+data "external" "authorize_policy_for_image" {
+  depends_on = ["data.ibm_is_vpc.f5_vpc"]
+  program    = ["bash", "${path.module}/scripts/create_auth.sh"]
+
+  query = {
+    ibmcloud_endpoint           = "${var.ibmcloud_endpoint}"
+    ibmcloud_vnf_svc_api_key    = "${var.ibmcloud_vnf_svc_api_key}"
+    source_service_account      = "${local.user_acct_id}"
+    source_service_name         = "is"
+    source_resource_type        = "image"
+    target_service_name         = "cloud-object-storage"
+    target_resource_type        = "bucket"
+    roles                       = "Reader"
+    target_resource_instance_id = "${var.vnf_f5bigip_cos_instance_id}"
+    region                      = "${data.ibm_is_region.region.name}"
+    resource_group_id           = "${data.ibm_resource_group.rg.id}"
+  }
 }
 
 resource "ibm_is_image" "f5_custom_image" {
-  count = "${var.copy_f5_image == "y" ? 1: 0}"
-  depends_on       = ["ibm_iam_authorization_policy.authorize_image"]
+  count            = "${var.copy_f5_image == "y" ? 1 : 0}"
+  depends_on       = ["data.external.authorize_policy_for_image"]
   href             = "${var.vnf_f5bigip_cos_image_url}"
   name             = "${var.f5_image_name}"
   operating_system = "centos-7-amd64"
@@ -48,7 +68,23 @@ resource "ibm_is_image" "f5_custom_image" {
   }
 }
 
+data "external" "delete_auth_policy_for_image" {
+  depends_on = ["ibm_is_image.f5_custom_image"]
+  program    = ["bash", "${path.module}/scripts/delete_auth.sh"]
+
+  query = {
+    id                       = "${lookup(data.external.authorize_policy_for_image.result, "id")}"
+    ibmcloud_endpoint        = "${var.ibmcloud_endpoint}"
+    ibmcloud_vnf_svc_api_key = "${var.ibmcloud_vnf_svc_api_key}"
+    region                   = "${data.ibm_is_region.region.name}"
+  }
+}
+
 data "ibm_is_image" "f5_custom_image" {
-  name="${var.f5_image_name}"
-  depends_on=["ibm_is_image.f5_custom_image"]
+  name       = "${var.f5_image_name}"
+  depends_on = ["ibm_is_image.f5_custom_image"]
+}
+
+output "auth_policy_id" {
+  value = "${lookup(data.external.authorize_policy_for_image.result, "id")}"
 }
