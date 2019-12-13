48e251d62a12bbbee0294df02e4a0815a46be402
Malar Kandasamy <k.malarvizhi@in.ibm.com>
2019-12-13 12:20:11 +0530

    add copy f5 image comment

--- variables.tf
+++ variables.tf
@@ -62,10 +62,14 @@ variable "f5_image_name" {
   description = "The name of the F5-BIGIP custom image to be provisioned in your IBM Cloud account."
 }
 
+##############################################################################
+# copy_f5_image - Do you want to copy the f5 image to your IBM Cloud account. Skip copy image, if your image is already copied.
+##############################################################################
 variable "copy_f5_image" {
   default     = "y"
   description = "Copy F5-BIGIP custom image to your IBM Cloud account (y/n)."
 }
+
 ##############################################################################
 # f5_image_name - The name of your F5-BIGIP Virtual Server to be provisioned
 ##############################################################################
